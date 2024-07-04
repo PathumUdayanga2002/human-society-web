9dfd7a32ed4ff056311af31777a5d68a5a7e0af7
blackdevil <madushanchathuranghe192@gmail.com>
2024-07-04 13:52:09 +0530

    meetExco update

--- src/components/MeetExco/MeetExco.jsx
+++ src/components/MeetExco/MeetExco.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const MeetExco = () => {
   return (
-    <div>MeetExco</div>
+    <div>
+      <div class="min-w-full bg-[url('/src/assets/hero1.jpg')] bg-cover bg-center min-h-screen sm:min-h-[200px] md:min-h-[500px] lg:min-h-[700px] xl:min-h-[900px] flex items-center justify-center">
+        <div className='text-3xl'>MeetExco</div>
+      </div>
+    </div>
   )
 }
 
