8cd61d0cd3b195c7c05a023e4202469b4eaf7003
Kushitha Lakshitha <168770766+Kushitha1521@users.noreply.github.com>
2024-07-17 17:13:15 +0530

    AboutUsHome

---
+++ src/components/AboutUsHome/AboutUsHome.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Link } from "react-router-dom";
+const AboutUsHome = () => {
+    return (
+        <div >
+            <Link to='/about'><div className="h-[200 px]">about us</div></Link>
+            
+        </div>
+
+    );
+};
+export default AboutUsHome
