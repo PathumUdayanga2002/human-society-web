e33c436fc5fb9035d48bdbe1f5c9c674f9e8ade2
maneeshaYasinth <maneeshayasinthgunarathna@gmail.com>
2024-11-01 13:49:56 +0530

    added diamonds icons to the home

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -25,14 +25,23 @@ const Home = () => {
         <div className='text-left text-white text-opacity-90 p-4 md:p-8'>
           <p className='text-xl md:text-3xl font-inter'>DISCOVER</p>
           <p className='2xl:text-12xl xl:text-9xl lg:text-9xl md:text-8xl sm:text-7xl text-4xl font-bold tracking-widest font-inter'>LIFE FOR A PAW MAGAZINE</p>
-          <p className='mt-4 text-sm md:text-xl leading-relaxed font-inter' >
+          <p className='mt-4 text-sm md:text-xl leading-relaxed font-inter'>
             HUMANE SOCIETY'S BIGGEST PROJECT REVOLVING AROUND ANIMAL HEALTHCARE AND WELFARE... 
             <br />AND WHAT ELSE. I NEED SOMETHING TO FILL UP THIS BLANK SPACE.
           </p>
           <p className='font-bold text-base mt-3'>SEE MORE</p>
         </div>
         <div className='absolute left-0 top-0 border-none py-[325px] xl:px-[350px] lg:px-[280px] bg-white bg-opacity-20'></div>
-
+        
+        {/* Diamond indicators */}
+        <div className='absolute bottom-6 right-12 flex space-x-6'>
+          {images.map((_, index) => (
+            <div
+              key={index}
+              className={`w-4 h-4 transform rotate-45 border-2 border-white ${currentImage === index ? 'bg-white' : 'bg-transparent'}`}
+            ></div>
+          ))}
+        </div>
       </div>
     </div>
   );
