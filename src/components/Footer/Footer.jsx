78b532dd15fc5b5f028ae4eb7fef23fc304f61c2
Kushitha Lakshitha <168770766+Kushitha1521@users.noreply.github.com>
2025-01-24 15:21:51 +0530

    links update

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,7 +3,7 @@ import fbLogo from '../../assets/fb.png'
 import instaLogo from '../../assets/insta.png'
 import linkedinLogo from '../../assets/linkedin-app-white-icon.png'
 import ytLogo from '../../assets/yt.png'
-
+import { Link } from "react-router-dom";
 
 
 const Footer = () => {
@@ -11,16 +11,16 @@ const Footer = () => {
     <footer className="bg-black text-white py-8">
     <div className="container mx-auto flex flex-col items-center space-y-4 sm:space-y-6 lg:space-y-8">
       <div className="flex space-x-8">
-        <a href="#" className="text-white text-xl sm:text-2xl"><img src={fbLogo} alt="Facebook" className="w-6 h-6 sm:w-8 sm:h-8" /></a>
-        <a href="#" className="text-white text-xl sm:text-2xl"><img src={instaLogo} alt="Facebook" className="w-6 h-6 sm:w-8 sm:h-8" /></a>
-        <a href="#" className="text-white text-xl sm:text-2xl"><img src={linkedinLogo} alt="Facebook" className="w-6 h-6 sm:w-8 sm:h-8" /></a>
-        <a href="#" className="text-white text-xl sm:text-2xl"><img src={ytLogo} alt="Facebook" className="w-6 h-6 sm:w-8 sm:h-8" /></a>
+        <a href="https://www.facebook.com/humaneUok/" className="text-white text-xl sm:text-2xl"><img src={fbLogo} alt="Facebook" className="w-6 h-6 sm:w-8 sm:h-8" /></a>
+        <a href="https://www.instagram.com/humane_uok/" className="text-white text-xl sm:text-2xl"><img src={instaLogo} alt="Facebook" className="w-6 h-6 sm:w-8 sm:h-8" /></a>
+        <a href="https://www.linkedin.com/company/humane-society-of-university-of-kelaniya/posts/?feedView=all" className="text-white text-xl sm:text-2xl"><img src={linkedinLogo} alt="Facebook" className="w-6 h-6 sm:w-8 sm:h-8" /></a>
+        <a href="https://www.youtube.com/@humanesocietyuok4169" className="text-white text-xl sm:text-2xl"><img src={ytLogo} alt="Facebook" className="w-6 h-6 sm:w-8 sm:h-8" /></a>
       </div>
       <div className="flex flex-wrap justify-center space-x-4 sm:space-x-6 lg:space-x-8">
-        <a href="#" className="text-white text-sm sm:text-base">ABOUT US</a>
+        <Link to={"/about"} ><a href="#" className="text-white text-sm sm:text-base">ABOUT US</a></Link>
         <a href="#" className="text-white text-sm sm:text-base">OUR PROJECTS</a>
-        <a href="#" className="text-white text-sm sm:text-base">NEWS</a>
-        <a href="#" className="text-white text-sm sm:text-base">DONATE US</a>
+         <Link to={"/news"}><a href="#" className="text-white text-sm sm:text-base">NEWS</a></Link>
+         <Link to={"/donate"}><a href="#" className="text-white text-sm sm:text-base">DONATE US</a></Link>
       </div>
       <hr className="w-full border-t-2 border-white my-4 sm:my-6 lg:my-8" />
 
